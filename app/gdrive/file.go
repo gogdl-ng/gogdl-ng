a72e29b916192c7ff3e24f798d2aff96683bf9a1
LegendaryB <6391022+LegendaryB@users.noreply.github.com>
2022-02-17 23:50:42 +0100

    Added transfer retry logic

--- app/gdrive/file.go
+++ app/gdrive/file.go
@@ -9,71 +9,74 @@ import (
 
 	"github.com/LegendaryB/gogdl-ng/app/logging"
 	"github.com/LegendaryB/gogdl-ng/app/utils"
+	"github.com/avast/retry-go"
 	"google.golang.org/api/drive/v3"
 )
 
 var logger = logging.NewLogger()
 
 func DownloadFile(folderPath string, driveFile *drive.File) error {
-	logger.Infof("starting to download file (name: %s, id: %s)", driveFile.Name, driveFile.Id)
+	return retry.Do(func() error {
+		logger.Infof("starting to download file (name: %s, id: %s)", driveFile.Name, driveFile.Id)
 
-	fp := filepath.Join(folderPath, driveFile.Name)
-	file, err := getLocalFile(fp, driveFile.Size)
+		fp := filepath.Join(folderPath, driveFile.Name)
+		file, err := getLocalFile(fp, driveFile.Size)
 
-	if err != nil {
-		logger.Errorf("failed to acquire local file: %w", err)
-		return err
-	}
+		if err != nil {
+			logger.Errorf("failed to acquire local file: %w", err)
+			return err
+		}
 
-	defer file.Close()
+		defer file.Close()
 
-	request := service.Files.Get(driveFile.Id).
-		SupportsAllDrives(true).
-		SupportsTeamDrives(true)
+		request := service.Files.Get(driveFile.Id).
+			SupportsAllDrives(true).
+			SupportsTeamDrives(true)
 
-	fi, err := file.Stat()
+		fi, err := file.Stat()
 
-	if err != nil {
-		logger.Errorf("failed to stat() file. %w", err)
-		return err
-	}
+		if err != nil {
+			logger.Errorf("failed to stat() file. %w", err)
+			return err
+		}
 
-	if fi.Size() == driveFile.Size {
-		logger.Infof("file is already finished. skipping..")
+		if fi.Size() == driveFile.Size {
+			logger.Infof("file is already finished. skipping..")
+			return nil
+		}
+
+		request.Header().Add("Range", fmt.Sprintf("bytes=%d-", fi.Size()))
+
+		response, err := request.Download()
+
+		if err != nil {
+			logger.Errorf("failed to fetch file. %w", err)
+			return err
+		}
+
+		_, err = io.Copy(file, response.Body)
+
+		if err != nil {
+			logger.Errorf("failed to write buffer to file. %w", err)
+			return err
+		}
+
+		md5checksum, err := utils.GetMd5Checksum(fp)
+
+		if err != nil {
+			logger.Errorf("failed to calculate md5 checksum. %w", err)
+			return err
+		}
+
+		if md5checksum != driveFile.Md5Checksum {
+			err = errors.New("md5 checksum mismatch")
+			logger.Errorf("the md5 checksum of the local file does not match checksum of the remote file. %w", err)
+			return err
+		}
+
+		logger.Info("finished file download")
 		return nil
-	}
-
-	request.Header().Add("Range", fmt.Sprintf("bytes=%d-", fi.Size()))
-
-	response, err := request.Download()
-
-	if err != nil {
-		logger.Errorf("failed to fetch file. %w", err)
-		return err
-	}
-
-	_, err = io.Copy(file, response.Body)
-
-	if err != nil {
-		logger.Errorf("failed to write buffer to file. %w", err)
-		return err
-	}
-
-	md5checksum, err := utils.GetMd5Checksum(fp)
-
-	if err != nil {
-		logger.Errorf("failed to calculate md5 checksum. %w", err)
-		return err
-	}
-
-	if md5checksum != driveFile.Md5Checksum {
-		err = errors.New("md5 checksum mismatch")
-		logger.Errorf("the md5 checksum of the local file does not match checksum of the remote file. %w", err)
-		return err
-	}
-
-	logger.Info("finished file download")
-	return nil
+	}, retry.Attempts(5))
 }
 
 func getLocalFile(path string, maxSize int64) (*os.File, error) {
